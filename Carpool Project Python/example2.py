a820bba699774e28df322cf2d51b1f24ad7b0d0b
Yusdivia Molina <yus.molina@gmail.com>
2021-11-18 20:21:24 -0600

    inicializar algunas clases

---
+++ Carpool Project Python/example2.py
@@ -0,0 +1,17 @@
+from example import *
+import matplotlib.pyplot as plt
+
+#inicializa 5 pasajeros
+passenger_model = PassengerModel(5)
+passenger_model.step()
+
+#inicializa 5 coches
+car_model = CarModel(5)
+car_model.step()
+
+traffic_light_model = TrafficLightModel(5)
+traffic_light_model.step()
+
+# para recorrer la lista de agentes
+for agent in passenger_model.schedule.agents:
+    print(agent.needs_ride())
